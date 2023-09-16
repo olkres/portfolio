8260a12e99ad7e3b4c2cdf97e8fcffc35bf462b3
olkres <belirium29@gmail.com>
2023-09-16 20:09:51 +0300

    feat: made js video player

--- index.js
+++ index.js
@@ -44,3 +44,113 @@ function changeImage(event) {
   }
 }
 portfolioBtns.addEventListener('click', changeImage);
+
+//Видеоплеер
+
+const player = document.querySelector('.video-player');
+const poster = document.querySelector('.poster');
+//далее константы обращаются к player и ищут элемент внутри .video-player
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const volume = player.querySelector('.volume');
+const volumeIcon = player.querySelector('.volume-icon');
+const toggle = player.querySelector('.play');
+const playBtn = player.querySelector('.btn-play');
+const time = player.querySelector('.time');
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';//тернарный оператор
+  video[method]();
+  //аналогичное действие
+  // if (video.paused) {
+  //   video.play()
+  // } else {
+  //   video.pause()
+  // }
+}
+
+function toggleMute() {
+  if(video.muted) {
+    video.muted = false;
+    volumeIcon.classList.remove('mute');
+  } else {
+    video.muted = true;
+    volumeIcon.classList.add('mute');
+  }
+}
+volumeIcon.addEventListener('click', toggleMute);
+
+function updateButton() {
+  if(this.paused) {
+    toggle.classList.remove('pause');
+    playBtn.style.display = 'block';
+  } else {
+    toggle.classList.add('pause');
+    playBtn.style.display = 'none';
+  }
+}
+
+function inputRange(input) {
+  const value = input.value;  
+  input.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #C8C8C8 ${value}%, #C8C8C8 100%)`;
+}
+progress.addEventListener('input', () => inputRange(progress));
+volume.addEventListener('input', () => inputRange(volume));
+
+
+function changeVolume() {
+	video.volume = this.value / 100;  
+  if(video.volume <= 0.01) {
+    video.muted = true;
+    volumeIcon.classList.add('mute');
+  } else {
+    video.muted = false;
+    volumeIcon.classList.remove('mute');
+  }
+}
+volume.addEventListener('input', changeVolume);
+
+function changeProgress() {
+	video.currentTime = this.value / 100 * video.duration;
+  if(video.currentTime < video.duration) {
+    video.play();
+  }
+}
+progress.addEventListener('input', changeProgress);
+
+function timer() {
+  progress.value = (video.currentTime / video.duration) * 100;
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) {
+    minutes = '0' + String(minutes);
+  }
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = '0' + String(seconds);
+  }
+  time.innerHTML = `${minutes}:${seconds}`;
+}
+
+video.addEventListener('timeupdate', timer);
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100 || 0;
+  progress.value = percent;
+  inputRange(progress);
+}
+
+function removePoster() {
+  poster.style.opacity = '0';
+  poster.style.pointerEvents = 'none';
+  setTimeout(function() {poster.style.display = 'none';}, 1000)
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton); 
+video.addEventListener('timeupdate', handleProgress);
+playBtn.addEventListener('click', togglePlay);
+playBtn.addEventListener('click', removePoster);
+poster.addEventListener('click', removePoster);
+poster.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
